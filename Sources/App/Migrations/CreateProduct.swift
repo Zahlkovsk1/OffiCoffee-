55c8cfa2475d6bc972632999f698c82aa8b50b2f
Shohjakhon Mamadaliev <shohjakhon.mamadaliev@ipratico.it>
2025-03-04 17:37:05 +0100

    new migration structure

--- Sources/App/Migrations/CreateProduct.swift
+++ Sources/App/Migrations/CreateProduct.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Fluent
+import Vapor
+
+
+struct CreateProduct: Migration {
+    
+    func prepare(on database: FluentKit.Database) -> NIOCore.EventLoopFuture<Void> {
+        database.schema ("products")
+            .id()
+            .field("title", .string, .required)
+            .field("description", .string, .required)
+            .field( "price", .int, .required)
+            .field("category", .string, .required)
+            .field("image", .string, .required)
+            .create()
+    }
+func revert(on database: FluentKit.Database) -> NIOCore.EventLoopFuture<Void> {
+    database.schema("products").delete()
+  }
+}
