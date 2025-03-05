671d9dbd78d1a1205d0e953931471e101ba3c811
Shohjakhon Mamadaliev <shohjakhon.mamadaliev@ipratico.it>
2025-03-05 10:35:39 +0100

    created migration for User

--- Sources/App/Migrations/CreateProduct.swift
+++ Sources/App/Migrations/CreateProduct.swift
@@ -13,9 +13,7 @@ import Vapor
 struct CreateProduct: AsyncMigration {
     
     func prepare(on database: FluentKit.Database) async throws {
-        
         let schema = database.schema("products")
-        
             .id()
             .field("title", .string, .required)
             .field("description", .string, .required)
---
+++ Sources/App/Migrations/CreateUser.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  restaurant
+//
+//  Created by Shohjakhon Mamadaliev on 05/03/25.
+//
+
+import Fluent
+import Vapor
+
+struct CreateUser: AsyncMigration {
+    
+    func prepare(on database: any FluentKit.Database) async throws {
+        
+        let schema = database.schema("users")
+            .id()
+            .field("name", .string, .required)
+            .field("login", .string, .required)
+            .field( "password", .string, .required)
+            .field("role", .string, .required)
+            .field("imagePic", .string)
+        try await  schema.create()
+        
+    }
+    
+    func revert(on database: any FluentKit.Database) async throws {
+       try await database.schema("users").delete()
+    }
+}
